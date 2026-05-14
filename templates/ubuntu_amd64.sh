8fd8a31a0a6a2112a7ad9188551aadaba7d82836
Iago Lucas <faria.iagolff@gmail.com>
2026-05-14 19:26:50 -0300

    adicionando infra projeto

---
+++ templates/ubuntu_amd64.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+wget https://amazoncloudwatch-agent.s3.amazonaws.com/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+dpkg -i -E ./amazon-cloudwatch-agent.deb
+/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a start   a
